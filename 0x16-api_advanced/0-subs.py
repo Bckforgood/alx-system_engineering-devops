b08740b3c3321d65a27d118a316633b38d7c0369
Bckforgood <t.abdo.khbaza@gmail.com>
2023-11-07 15:38:50 -0800

    fix errors

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """
 Function that queries the Reddit API and returns the number of subscribers
-(not active users, total subscribers) for a given subreddit.
+(not active users, total subscribers).
 If an invalid subreddit is given, the function should return 0
 """
 
@@ -9,10 +9,7 @@ import requests
 
 
 def number_of_subscribers(subreddit):
-    """
-    Function that queries the Reddit API
-    - If not a valid subreddit, return 0.
-    """
+    
     req = requests.get(
         "https://www.reddit.com/r/{}/about.json".format(subreddit),
         headers={"User-Agent": "Custom"},
